d7d9fbbe31e1a00f2ba000bc0de178df43d91a82
domotheawsome <cblappers@gmail.com>
2022-06-19 21:44:26 -0700

    color flipper good for now

--- color-flipper3/src/components/ColorFlipper.js
+++ color-flipper3/src/components/ColorFlipper.js
@@ -1,10 +1,30 @@
 import React from "react"
 
 export default function ColorFlipper() {
+    const [color, setColor] = React.useState('white')
+    const [textColor, setTextColor] = React.useState('black')
+
+
+    function toggleColor() {
+        var randomColor = Math.floor(Math.random()*16777215);
+        var newTextColor = (randomColor > 16200000) ? 'black' : 'white'
+
+        setTextColor(newTextColor)
+
+        randomColor = "#" + randomColor.toString(16);
+
+        setColor(randomColor)
+        return 0
+
+    }
+
     return(
-        <div className="flex h-screen m-auto">
-            <div className="m-auto">
-                <button className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded scale-150">Click Me!</button>
+
+        <div style={{backgroundColor: color}} className="flex h-screen m-auto">
+            <div className="m-auto space-y-4">
+                <button className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded scale-150" onClick={toggleColor}>Click Me!</button>
+
+                <p style={{color: textColor}}>Color: {color}</p>
             </div>
         </div>
     )
