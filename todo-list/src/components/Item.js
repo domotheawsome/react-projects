c9202e87f22deabbbb5ab69798b9e963c1c4941a
domotheawsome <cblappers@gmail.com>
2022-07-14 16:35:39 -0700

    cleaned up hooks in todolist

--- todo-list/src/components/Item.js
+++ todo-list/src/components/Item.js
@@ -10,42 +10,31 @@ export default function ColorFlipper() {
 
         // className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded scale-150"
 
+    const displayItem = itemArray.map(item => { 
+        return (<li key={item.nextId}>{item.item}{' '}<button onClick={() => deleteItem(item)}>
+        Delete</button></li>)})
+
 
     function addItem () {
             setItem('')
             setItemArray([...itemArray, { nextId: nextId++, item: item }])
     }
-
-
-
-    
+        
+    function deleteItem(item) {
+            setItemArray(itemArray.filter(i => (i.nextId!==item.nextId)))
+    }
+   
     return(
         <div className="flex flex-col flex-shrink-0 ml-6">
             <p>Enter what you want</p>
             <div className="py-6 space-x-4 ">
                 <input className="form-control text-base bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                
         
                 value={item} onChange={e => setItem(e.target.value)} />
 
                 <button onClick={addItem}>{console.log(itemArray)}Add</button>
 
-
-                <ul>
-                    {itemArray.map(item=> (
-                        <li key={item.nextId}>{item.item}{' '}
-                            <button 
-                                onClick={() => 
-                                    {
-                                        setItemArray(itemArray.filter(i => (i.nextId!==item.nextId)))
-                                    }
-                                }
-                            >
-                                Delete
-                            </button>
-                        </li>
-                    ))}
-                </ul>
+                <ul>{displayItem}</ul>
 
             </div>
         </div>
