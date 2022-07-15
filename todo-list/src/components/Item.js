8c36280bbbc5fc467304c58e9fe3ec851e8c10ac
domotheawsome <cblappers@gmail.com>
2022-07-14 17:17:32 -0700

    added styling todolist

--- todo-list/src/components/Item.js
+++ todo-list/src/components/Item.js
@@ -11,7 +11,7 @@ export default function ColorFlipper() {
         // className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded scale-150"
 
     const displayItem = itemArray.map(item => { 
-        return (<li key={item.nextId}>{item.item}{' '}<button onClick={() => deleteItem(item)}>
+        return (<li className="button--style" key={item.nextId}>{item.item}{' '}<button className="button--delete" onClick={() => deleteItem(item)}>
         Delete</button></li>)})
 
 
@@ -25,14 +25,14 @@ export default function ColorFlipper() {
     }
    
     return(
-        <div className="flex flex-col flex-shrink-0 ml-6">
-            <p>Enter what you want</p>
-            <div className="py-6 space-x-4 ">
-                <input className="form-control text-base bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        <div className="flex flex-col flex-shrink-0 ml-20">
+            <p>Add a task:</p>
+            <div className="py-6">
+                <input className="form-control text-base bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none "
         
                 value={item} onChange={e => setItem(e.target.value)} />
 
-                <button onClick={addItem}>{console.log(itemArray)}Add</button>
+                <button className="ml-3 mb-6" onClick={addItem}>{console.log(itemArray)}Add</button>
 
                 <ul>{displayItem}</ul>
 
