601ddaf865476db1f3118c8ecd5011d8ee743b46
domotheawsome <cblappers@gmail.com>
2022-08-23 15:15:52 -0700

    starting calculator

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Display from './components/Display';
+import Keypad from './components/Keypad';
+
+function App() {
+  return (
+    <div>
+      <Display />
+      <Keypad />
+    </div>
+  );
+}
+
+export default App;
