ac1e243fea116efc8cfe61ab5179f1955ae70852
domotheawsome <cblappers@gmail.com>
2023-02-06 23:16:29 -0800

    nav styling

--- mock-database/src/components/Nav.js
+++ mock-database/src/components/Nav.js
@@ -9,7 +9,7 @@ export default function Nav() {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-            <p className="navbar-brand">StarWars DB</p>
+            <p className="navbar-brand marg">Star Wars DB</p>
             
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
